92e4dedeec80f050f1b702288c0b07e78451996a
ctn <ctnhan2002@gmail.com>
2022-10-15 21:26:04 +0700

    .

--- Forms/Manage/frm_ThemSuaNhanVien.Designer.cs
+++ Forms/Manage/frm_ThemSuaNhanVien.Designer.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿namespace FruitApp.Forms.Manage
 {
     partial class frm_ThemSuaNhanVien
@@ -43,4 +44,51 @@
 
         #endregion
     }
+=======
+﻿namespace FruitApp.Forms.Manage
+{
+    partial class frm_ThemSuaNhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm_ThemSuaNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1154, 530);
+            this.Name = "frm_ThemSuaNhanVien";
+            this.Text = "Thêm/Sửa Thông Tin Nhân Viên";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+>>>>>>> c22e5a39921815c4b480d67e1584fa894bd14d47
 }
