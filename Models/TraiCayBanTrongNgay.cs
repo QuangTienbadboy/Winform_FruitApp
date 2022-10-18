c5f0871b662aaae4e6aa4850cdb9e75ad107b64a
ctn2002 <61001185+ctn2002@users.noreply.github.com>
2022-10-18 20:53:03 +0700

    Add files via upload

---
+++ Models/TraiCayBanTrongNgay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitApp.Models
+{
+    internal class TraiCayBanTrongNgay
+    {
+        public string TenTraiCay { get; set; }
+        public int SoLuong { get; set; }
+    }
+}
