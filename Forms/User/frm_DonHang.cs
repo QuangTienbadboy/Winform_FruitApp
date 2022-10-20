743c491faa8a1887e3dc2978495b793d9d226c8a
QuangTienbadboy <114653411+QuangTienbadboy@users.noreply.github.com>
2022-10-20 14:45:23 +0700

    Update frm_DonHang.cs

--- Forms/User/frm_DonHang.cs
+++ Forms/User/frm_DonHang.cs
@@ -1,4 +1,5 @@
-using DoAn.Models;
+
+using FruitApp.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -19,49 +20,57 @@ namespace DoAn
         {
             InitializeComponent();
         }
-
-        //private void loadDataGridView(string MaTK)
-        //{
-        //    try
-        //    {
-        //        DataGw3.Rows.Clear();
-        //        FruitAppContext ctx = new FruitAppContext();
-        //        List<DonHang> ldh = ctx.DonHangs.Where(p => p.MaTaiKhoan == MaTK);
-        //        if (ldh.Count > 0)
-        //        {
-        //            foreach (var dh in ldh)
-        //            {
-        //                int index = DataGw3.Rows.Add();
-        //                DataGw3.Rows[index].Cells[0].Value = dh.MaDonHang;
-        //                string s = "";
-        //                List<ChiTietDonHang> lct = ctx.ChiTietDonHangs.Where(p => p.MaDonHang == dh.MaDonHang).ToList();
-        //                int d = 0;
-        //                foreach (var ct in lct)
-        //                {
-        //                    if (d != lct.Count - 1)
-        //                    {
-        //                        s = s + ct.TraiCay.TenTraiCay + " " + ct.TraiCay.DonViTinh + "x" + ct.SoLuong + ", ";
-        //                        d++;
-        //                    }
-        //                    else
-        //                        s = s + ct.TraiCay.TenTraiCay + " " + ct.TraiCay.DonViTinh + "x" + ct.SoLuong + ".";
-        //                }
-        //                da.Rows[index].Cells[1].Value = s;
-        //                dgv_Order.Rows[index].Cells[2].Value = dh.ThanhTien;
-        //                dgv_Order.Rows[index].Cells[3].Value = dh.NgayDatHang.ToShortDateString();
-        //                dgv_Order.Rows[index].Cells[4].Value = dh.TrangThai;
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show(ex.Message, "Thông Báo");
-        //    }
-        //}
-
-        private void ĐơnHàng_Load(object sender, EventArgs e)
+        internal string matk;
+        private void loadDataGridView(string MaTK)
         {
+            try
+            {
+                DataGw3.Rows.Clear();
+                FruitAppContext ctx = new FruitAppContext();
+                List<DonHang> ldh = ctx.DonHangs.Where(p => p.TaiKhoanKhachHang.MaKhachHang == MaTK).ToList();
+                if (ldh.Count > 0)
+                {
+                    foreach (var dh in ldh)
+                    {
+                        int index = DataGw3.Rows.Add();
+                        DataGw3.Rows[index].Cells[0].Value = dh.MaDonHang;
+                        string s = "";
+                        List<ChiTietDonHang> lct = ctx.ChiTietDonHangs.Where(p => p.MaDonHang == dh.MaDonHang).ToList();
+                        int d = 0;
+                        foreach (var ct in lct)
+                        {
+                            if (d != lct.Count - 1)
+                            {
+                                s = s + ct.TraiCay.TenTraiCay + " " + ct.TraiCay.DonViTinh + "x" + ct.SoLuong + ", ";
+                                d++;
+                            }
+                            else
+                                s = s + ct.TraiCay.TenTraiCay + " " + ct.TraiCay.DonViTinh + "x" + ct.SoLuong + ".";
+                        }
+                        DataGw3.Rows[index].Cells[1].Value = s;
+                        DataGw3.Rows[index].Cells[2].Value = dh.ThanhTien;
+                        DataGw3.Rows[index].Cells[3].Value = dh.NgayDatHang.ToShortDateString();
+                        DataGw3.Rows[index].Cells[4].Value = dh.TrangThai;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Thông Báo");
+            }
+        }
 
+       
+
+        private void frm_Donhang_Load(object sender, EventArgs e)
+        {
+            loadDataGridView(matk);
+        }
+
+        private void btThoat_Click(object sender, EventArgs e)
+        {
+            this.Owner.Show();
+            this.Close();
         }
     }
 }
