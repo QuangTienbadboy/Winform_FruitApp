3833d091b84de4b7e134a07babe3cb0debb35688
QuangTienbadboy <114653411+QuangTienbadboy@users.noreply.github.com>
2022-10-20 10:42:51 +0700

    Update frm_DiaChi.Designer.cs

--- Forms/User/frm_DiaChi.Designer.cs
+++ Forms/User/frm_DiaChi.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace FruitApp.Forms.User
+namespace DoAn
 {
-    partial class frm_DiaChi
+    partial class DiaChi
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtTongTien = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btDatHang = new System.Windows.Forms.Button();
+            this.btThoat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtDiaChiNhanHang = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtTongTien
+            // 
+            this.txtTongTien.Location = new System.Drawing.Point(277, 125);
+            this.txtTongTien.Name = "txtTongTien";
+            this.txtTongTien.Size = new System.Drawing.Size(302, 22);
+            this.txtTongTien.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(73, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Địa Chỉ Nhận Hàng";
+            // 
+            // btDatHang
+            // 
+            this.btDatHang.Location = new System.Drawing.Point(277, 191);
+            this.btDatHang.Name = "btDatHang";
+            this.btDatHang.Size = new System.Drawing.Size(95, 39);
+            this.btDatHang.TabIndex = 3;
+            this.btDatHang.Text = "Đặt Hàng ";
+            this.btDatHang.UseVisualStyleBackColor = true;
+            this.btDatHang.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btThoat
+            // 
+            this.btThoat.Location = new System.Drawing.Point(484, 191);
+            this.btThoat.Name = "btThoat";
+            this.btThoat.Size = new System.Drawing.Size(95, 39);
+            this.btThoat.TabIndex = 4;
+            this.btThoat.Text = "Thoát";
+            this.btThoat.UseVisualStyleBackColor = true;
+            this.btThoat.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tổng Tiền";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(382, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 39);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nhập Địa chỉ";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtDiaChiNhanHang);
+            this.panel1.Controls.Add(this.txtTongTien);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btDatHang);
+            this.panel1.Controls.Add(this.btThoat);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(16, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(898, 250);
+            this.panel1.TabIndex = 8;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // txtDiaChiNhanHang
+            // 
+            this.txtDiaChiNhanHang.Location = new System.Drawing.Point(277, 51);
+            this.txtDiaChiNhanHang.Name = "txtDiaChiNhanHang";
+            this.txtDiaChiNhanHang.Size = new System.Drawing.Size(302, 22);
+            this.txtDiaChiNhanHang.TabIndex = 6;
+            // 
+            // DiaChi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frm_DiaChi";
+            this.BackColor = System.Drawing.Color.YellowGreen;
+            this.BackgroundImage = global::DoAn.Properties.Resources.icons8_warehouse_64;
+            this.ClientSize = new System.Drawing.Size(926, 348);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Name = "DiaChi";
+            this.Text = "Địa Chỉ";
+            this.Load += new System.EventHandler(this.DiaChi_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btDatHang;
+        private System.Windows.Forms.Button btThoat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        internal System.Windows.Forms.TextBox txtTongTien;
+        internal System.Windows.Forms.TextBox txtDiaChiNhanHang;
     }
 }
