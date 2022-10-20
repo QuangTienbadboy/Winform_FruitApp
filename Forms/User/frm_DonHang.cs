ffe05f487fb692eb37b6e83edc92bcdec073e5a8
QuangTienbadboy <114653411+QuangTienbadboy@users.noreply.github.com>
2022-10-20 10:46:06 +0700

    Update frm_DonHang.cs

--- Forms/User/frm_DonHang.cs
+++ Forms/User/frm_DonHang.cs
@@ -1,20 +1,67 @@
-﻿using System;
+using DoAn.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FruitApp.Forms.User
+namespace DoAn
 {
-    public partial class frm_DonHang : Form
+    public partial class frm_Donhang : Form
     {
-        public frm_DonHang()
+        SqlConnection con;
+        public frm_Donhang()
         {
             InitializeComponent();
+        }
+
+        //private void loadDataGridView(string MaTK)
+        //{
+        //    try
+        //    {
+        //        DataGw3.Rows.Clear();
+        //        FruitAppContext ctx = new FruitAppContext();
+        //        List<DonHang> ldh = ctx.DonHangs.Where(p => p.MaTaiKhoan == MaTK);
+        //        if (ldh.Count > 0)
+        //        {
+        //            foreach (var dh in ldh)
+        //            {
+        //                int index = DataGw3.Rows.Add();
+        //                DataGw3.Rows[index].Cells[0].Value = dh.MaDonHang;
+        //                string s = "";
+        //                List<ChiTietDonHang> lct = ctx.ChiTietDonHangs.Where(p => p.MaDonHang == dh.MaDonHang).ToList();
+        //                int d = 0;
+        //                foreach (var ct in lct)
+        //                {
+        //                    if (d != lct.Count - 1)
+        //                    {
+        //                        s = s + ct.TraiCay.TenTraiCay + " " + ct.TraiCay.DonViTinh + "x" + ct.SoLuong + ", ";
+        //                        d++;
+        //                    }
+        //                    else
+        //                        s = s + ct.TraiCay.TenTraiCay + " " + ct.TraiCay.DonViTinh + "x" + ct.SoLuong + ".";
+        //                }
+        //                da.Rows[index].Cells[1].Value = s;
+        //                dgv_Order.Rows[index].Cells[2].Value = dh.ThanhTien;
+        //                dgv_Order.Rows[index].Cells[3].Value = dh.NgayDatHang.ToShortDateString();
+        //                dgv_Order.Rows[index].Cells[4].Value = dh.TrangThai;
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message, "Thông Báo");
+        //    }
+        //}
+
+        private void ĐơnHàng_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
