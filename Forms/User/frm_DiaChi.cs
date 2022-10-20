976d8c8829b317090eb8acc1007a7fd70f3846fc
QuangTienbadboy <114653411+QuangTienbadboy@users.noreply.github.com>
2022-10-20 10:44:11 +0700

    Update frm_DiaChi.cs

--- Forms/User/frm_DiaChi.cs
+++ Forms/User/frm_DiaChi.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,13 +8,51 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FruitApp.Forms.User
+namespace DoAn
 {
-    public partial class frm_DiaChi : Form
+    public partial class DiaChi : Form
     {
-        public frm_DiaChi()
+        public DiaChi()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t = 0;
+            MessageBox.Show("Bạn có muốn thoát không"," ",MessageBoxButtons.OK );
+            this.Close();
+        }
+        internal int t = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtDiaChiNhanHang.Text != "")
+                {
+                    t = 1;
+                    MessageBox.Show("Bạn Đã Đặt Hàng Thành Công");
+                    this.Close();
+                }
+                else
+                {
+                    throw new Exception("Bạn cần nhập địa chỉ");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Thông Báo");
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void DiaChi_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
